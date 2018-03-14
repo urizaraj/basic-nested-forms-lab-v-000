a8043a37e8770c169162819c4809bfa0feaf48cb
Alexander Urizar <urizaraj@gmail.com>
2018-03-14 18:07:16 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    recipe = Recipe.new(strong_params)
+    return render :new unless recipe.save
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+  def strong_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [:name, :quantity]
+    )
   end
 end
